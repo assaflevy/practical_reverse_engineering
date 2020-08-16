0f17a3b9ffd6c16ad1b7308138a50fbb29aa1b65
Assaf <34716025+assaflevy@users.noreply.github.com>
2020-08-16 23:36:38 +0300

    Update KeInitializeApc.cpp

--- chapter_1/page_35/exercise_5/KeInitializeApc.cpp
+++ chapter_1/page_35/exercise_5/KeInitializeApc.cpp
@@ -1,3 +1,34 @@
+//nt!KeInitializeApc:
+//fffff802`804c7a70 c60112          mov     byte ptr[rcx], 12h
+//fffff802`804c7a73 4c8bd1          mov     r10, rcx
+//fffff802`804c7a76 c6410258        mov     byte ptr[rcx + 2], 58h
+//fffff802`804c7a7a 4183f802        cmp     r8d, 2
+//fffff802`804c7a7e 7443            je      nt!KeInitializeApc + 0x53 (fffff802`804c7ac3)  Branch
+//
+//nt!KeInitializeApc + 0x10:
+//fffff802`804c7a80 488b442428      mov     rax, qword ptr[rsp + 28h]
+//fffff802`804c7a85 44884150        mov     byte ptr[rcx + 50h], r8b
+//fffff802`804c7a89 48894128        mov     qword ptr[rcx + 28h], rax
+//fffff802`804c7a8d 48895108        mov     qword ptr[rcx + 8], rdx
+//fffff802`804c7a91 488b542430      mov     rdx, qword ptr[rsp + 30h]
+//fffff802`804c7a96 48895130        mov     qword ptr[rcx + 30h], rdx
+//fffff802`804c7a9a 488bc2          mov     rax, rdx
+//fffff802`804c7a9d 48f7d8          neg     rax
+//fffff802`804c7aa0 4c894920        mov     qword ptr[rcx + 20h], r9
+//fffff802`804c7aa4 481bc9          sbb     rcx, rcx
+//fffff802`804c7aa7 48234c2440 and rcx, qword ptr[rsp + 40h]
+//fffff802`804c7aac 48f7da          neg     rdx
+//fffff802`804c7aaf 1ac0            sbb     al, al
+//fffff802`804c7ab1 22442438 and al, byte ptr[rsp + 38h]
+//fffff802`804c7ab5 41884251        mov     byte ptr[r10 + 51h], al
+//fffff802`804c7ab9 49894a38        mov     qword ptr[r10 + 38h], rcx
+//fffff802`804c7abd 41c6425200      mov     byte ptr[r10 + 52h], 0
+//fffff802`804c7ac2 c3              ret
+//
+//nt!KeInitializeApc + 0x53:
+//fffff802`804c7ac3 448a824a020000  mov     r8b, byte ptr[rdx + 24Ah]
+//fffff802`804c7aca ebb4            jmp     nt!KeInitializeApc + 0x10 (fffff802`804c7a80)  Branch
+
 typedef struct _KAPC
 {
     UCHAR Type;
