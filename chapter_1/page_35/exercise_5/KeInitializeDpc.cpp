c17fc0a42552bf7d9d14a3b8d9ff9a74af68bc8d
Assaf <34716025+assaflevy@users.noreply.github.com>
2020-08-16 23:37:39 +0300

    Update KeInitializeDpc.cpp

--- chapter_1/page_35/exercise_5/KeInitializeDpc.cpp
+++ chapter_1/page_35/exercise_5/KeInitializeDpc.cpp
@@ -1,15 +1,10 @@
-//nt!_KDPC
-//+ 0x000 TargetInfoAsUlong : Uint4B
-//+ 0x000 Type : UChar
-//+ 0x001 Importance : UChar
-//+ 0x002 Number : Uint2B
-//+ 0x008 DpcListEntry : _SINGLE_LIST_ENTRY
-//+ 0x010 ProcessorHistory : Uint8B
-//+ 0x018 DeferredRoutine : Ptr64     void
-//+ 0x020 DeferredContext : Ptr64 Void
-//+ 0x028 SystemArgument1 : Ptr64 Void
-//+ 0x030 SystemArgument2 : Ptr64 Void
-//+ 0x038 DpcData : Ptr64 Void
+//fffff802`8040b790 33c0 xor eax, eax
+//fffff802`8040b792 c70113010000    mov     dword ptr[rcx], 113h
+//fffff802`8040b798 48894138        mov     qword ptr[rcx + 38h], rax
+//fffff802`8040b79c 48894110        mov     qword ptr[rcx + 10h], rax
+//fffff802`8040b7a0 48895118        mov     qword ptr[rcx + 18h], rdx
+//fffff802`8040b7a4 4c894120        mov     qword ptr[rcx + 20h], r8
+//fffff802`8040b7a8 c3              ret
 
 // With help from https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ke/dpcobj/kdpc.htm
 typedef struct
@@ -31,15 +26,6 @@ typedef struct
     PVOID SystemArgument2;
     PVOID DpcData;
 } _KDPC, *PRKDPC;
-
-//nt!KeInitializeDpc:
-//fffff802`8040b790 33c0 xor eax, eax
-//fffff802`8040b792 c70113010000    mov     dword ptr[rcx], 113h
-//fffff802`8040b798 48894138        mov     qword ptr[rcx + 38h], rax
-//fffff802`8040b79c 48894110        mov     qword ptr[rcx + 10h], rax
-//fffff802`8040b7a0 48895118        mov     qword ptr[rcx + 18h], rdx
-//fffff802`8040b7a4 4c894120        mov     qword ptr[rcx + 20h], r8
-//fffff802`8040b7a8 c3              ret
 
 VOID
 NTAPI
